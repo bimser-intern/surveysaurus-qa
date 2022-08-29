57825639e1d3ef572fe77318f73f5a1f2832d8d4
Ummugulsum Erol <ummugulsumerol@marun.edu.tr>
2022-08-29 18:08:24 +0300

    Komut satırı argümanları eklendi.

--- SurveySaurusLogin1.py
+++ SurveySaurusLogin1.py
@@ -2,20 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('--disable-infobars')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--window-size=1920,1080')
-driver = webdriver.Chrome(options=options)
-
-
+import sys 
 
 #service = Service(".\\chromedriver.exe")
 #driver = webdriver.Chrome(service = service)
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+
+if len(sys.argv) == 1:
+    sys.exit()
+
+if sys.argv[1] == "production":
+ 
+    options.add_argument("headless")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
+ 
+
+if sys.argv[1] == "development":
+    # for development
+    pass
+
 driver.maximize_window()
 
 def login(username, password):
@@ -29,23 +40,25 @@ def login(username, password):
 
 login("123@gmail.com", "Ae123456")
 mesaj = driver.find_element(By.CLASS_NAME, "wrongLogIn").text
-if "Incorrect email or password" in mesaj:
+if "Incorrect email or password" == mesaj:
     print("Şifre hatalı girildiğinde doğru uyarı verilmiştir.")
 else:
     print("HATA: Şifre hatalı girilmesine rağmen uyarı verilmemiştir.")
 
 login("12345@gmail.com", "Ae123456")
 mesaj = driver.find_element(By.CLASS_NAME, "wrongLogIn").text
-if "Incorrect email or password" in mesaj:
+if "Incorrect email or password" == mesaj:
     print("Kullanıcı adı hatalı girildiğinde doğru uyarı verilmiştir.")
 else:
     print("HATA: Kullanıcı adı yanlış girilmesine rağmen uyarı verilmemiştir.")
 
 login("123@gmail.com", "Ue123456")
 time.sleep(5)
-if "userPage" in driver.current_url:
+if "https://survey-ui.orangeground-88d990d8.westeurope.azurecontainerapps.io/userPage" == driver.current_url:
     print("Kullanıcı adı ve şifre doğru yazıldığında başarılı bir şekilde giriş yapılmıştır. ")
 else:
     print("HATA: Kullanıcı adı ve şifre doğru yazıldığında başarılı bir şekilde giriş yapılamamıştır. ")
+
+
 
 driver.quit()
